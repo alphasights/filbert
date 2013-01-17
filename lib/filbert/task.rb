c14fe9cade2979be7a8b96d1c53b08b6df685923
Tadas Tamosauskas <tadas@pdfcv.com>
2013-01-17 16:03:52 +0000

    Move kill_connections to its own task

--- lib/filbert/task.rb
+++ lib/filbert/task.rb
@@ -29,22 +29,34 @@ module Filbert
     end
 
     method_option :config, type: :string, default: "config/database.yml"
-    method_option :env, type: :string, default: "production"
+    method_option :env, type: :string, default: "development"
     desc "restore", "restore the latest db dump"
     def restore
       most_recent_file = ordered_dumps.last
-      db_config = DbConfig.new(options[:config], options[:env])
-
       check_dump_ready(most_recent_file)
-      check_config_ready(db_config)
 
       say "Restoring: #{db_config.database} <--- #{most_recent_file.path}"
-      kill_connections(db_config.database, db_config.username)
+      invoke :kill_connections
       ENV['PGPASSWORD'] = db_config.password
       run! "pg_restore -U #{db_config.username} -d #{db_config.database} -w #{most_recent_file.path}"
 
     rescue Errno::ENOENT
       say "Could not find config file #{options[:config]}. Please pass in --config with a path to database.yml"
+    ensure
+      ENV['PGPASSWORD'] = nil
+    end
+
+    method_option :config, type: :string, default: "config/database.yml"
+    method_option :env, type: :string, default: "development"
+    desc "kill_connections", "Kills all open connections to the db"
+    def kill_connections
+      database = db_config.database
+      user = db_config.username
+
+      ENV['PGPASSWORD'] = db_config.password
+      sql = "SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE procpid <> pg_backend_pid();"
+      run! "echo \"#{sql}\" |  psql -d #{database} -U #{user}"
+      say "Killed connections to #{database} as #{user}"
     ensure
       ENV['PGPASSWORD'] = nil
     end
@@ -90,16 +102,15 @@ module Filbert
         end
       end
 
-      def check_config_ready(db_config)
-        if db_config.config.nil?
-          say "Could not find config for \"#{options[:env]}\" in #{options[:config]}"
-          exit 0
+      def db_config
+        @db_config ||= begin
+          db_config = DbConfig.new(options[:config], options[:env])
+          if db_config.config.nil?
+            say "Could not find config for \"#{options[:env]}\" in #{options[:config]}"
+            exit 0
+          end
+          db_config
         end
-      end
-
-      def kill_connections(database, user)
-        sql = "SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE procpid <> pg_backend_pid();"
-        run "echo #{sql} |  psql -d #{database} -U #{user}"
       end
   end
 end
