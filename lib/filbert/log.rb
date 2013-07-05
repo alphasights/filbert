7c73495fac55e66b904882404b5899da47e1a6ea
David Griffiths <david.griffiths@alphasights.com>
2013-07-05 12:31:41 +0100

    Change log to accept the dbname as well

--- lib/filbert/log.rb
+++ lib/filbert/log.rb
@@ -2,17 +2,20 @@ require 'rest_client'
 require 'json'
 
 class Log
-  def initialize(task_name)
+  def initialize(task_name, db_name)
     @task_name = task_name
+    @db_name = db_name
   end
 
   def success 
-    RestClient.put(datastore, payload.to_json) if datastore.present 
+    RestClient.put(datastore, payload.to_json) unless datastore.nil?
   rescue RestClient::Exception 
     #just dont explode the back up task
   end
 
   private
+
+  attr_reader :task_name, :db_name
 
   def datastore 
     ENV['FILBERT_LOG_URL']
@@ -26,7 +29,7 @@ class Log
   def payload 
     {
       "_rev" => rev,
-      "app"=> "pistachio",
+      "app"=> db_name,
       "timestamp"=> Time.now.to_s,
       "js_timestamp" => Time.now.to_i, 
       "task_name" => task_name
